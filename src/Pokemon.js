b2f438249e145cc85a1234e82e8178f66bd61481
brandon <lrbn86@gmail.com>
2017-06-20 19:04:25 -0400

    Setting up on using the Pokemon API

---
+++ src/Pokemon.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { Route, NavLink } from 'react-router-dom'
+import PokemonInfo from './PokemonInfo'
+import './Pokemon.css'
+
+class Pokemon extends Component {
+  render = () => {
+    return (
+      <div className="pokemon">
+        <img 
+          className="pokemon-logo" 
+          src="https://seeklogo.com/images/P/Pokemon-logo-497D61B223-seeklogo.com.png"
+          alt="pokemon"
+        />
+        <h2>Select a pokemon.</h2>
+        <ul className="nav-links">
+          <li>
+            <NavLink to='/pokemon/bulbasaur'>Bulbasaur</NavLink>
+          </li>
+          <li>
+            <NavLink to='/pokemon/charmander'>Charmander</NavLink>
+          </li>
+          <li>
+            <NavLink to='/pokemon/squirtle'>Squirtle</NavLink>
+          </li>
+        </ul>
+
+        <Route 
+          exact path={this.props.match.url} 
+          render={() => (<h2>No pokemon selected.</h2>)}
+        />
+
+        <Route
+          path={`${this.props.match.url}/:pokemon`} component={PokemonInfo}
+        />
+
+      </div>
+    )
+  }
+}
